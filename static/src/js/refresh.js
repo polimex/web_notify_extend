1fe1e05079530ca73872f928f83e4b8cc66c6578
Lubo <lgeorgiev@securitybulgaria.com>
2020-12-03 12:09:27 +0200

    fix

--- static/src/js/refresh.js
+++ static/src/js/refresh.js
@@ -16,33 +16,37 @@ WebClient.include({
 
     },
 	start_polling: function() {
-            this.call("bus_service", "startPolling");
-
-            if (this.call("bus_service", "isMasterTab")) {
-                this.call("bus_service", "addChannel", 'ichecker_refresh');
-            }
-            this.call("bus_service", "on", "notification", this, this.ichecker_refresh);
-        },
+		this.call("bus_service", "startPolling");
+		if (this.call("bus_service", "isMasterTab")) {
+			this.call("bus_service", "addChannel", 'ichecker_refresh');
+		}
+		this.call("bus_service", "on", "notification", this, this.ichecker_refresh);
+		// this.call("bus_service", "on", "ichecker_refresh", this, this.ichecker_refresh);
+    },
 	ichecker_refresh: function(messages) {
 		var self = this;
     	var action = this.action_manager.getCurrentAction();
     	var controller = this.action_manager.getCurrentController();
-    	// console.log('Received event: ',messages)
 		_.each(messages, function (m) {
-			if ((m.length > 1)&&(controller)){
+			if ((m.length > 1)&&(action)&&(controller)&&(m[0] == 'ichecker_refresh')){
 				// console.log('Received event: ',m[1])
-				if((controller.widget)&&(controller.widget.modelName == 'board.board')){
+				// console.log('Action: ',action)
+				// console.log('Controller: ',controller)
+				var isMasterTab = self.call('bus_service', 'isMasterTab')
+				// debugger
+				if (!isMasterTab || session.uid !== m[1].uid &&
+					 (controller.widget.modelName === m[1].model || controller.widget.isDashboard) &&
+					controller.widget.mode === "readonly") {
+						// var recordID = action.env.currentID || null; // pyUtils handles null value, not undefined
+						// if(controller.widget.isMultiRecord && (m[1].create || _.intersection(m[1].ids, action.env.ids) >= 1)) {
+						// 	self._reload(m[1], controller);
+						// } else if(!controller.widget.isMultiRecord && m[1].ids.includes(recordID)) {
+						// 	self._reload(m[1], controller);
+						// }
 					self._reload(m[1], controller);
 				} else
-				if (!self.call('bus_service', 'isMasterTab') || session.uid !== m[1].uid &&
-					action && controller && (controller.widget.modelName === m[1].model || controller.widget.isDashboard) &&
-					controller.widget.mode === "readonly") {
-						var recordID = action.env.currentID || null; // pyUtils handles null value, not undefined
-						if(controller.widget.isMultiRecord && (m[1].create || _.intersection(m[1].ids, action.env.ids) >= 1)) {
-							self._reload(m[1], controller);
-						} else if(!controller.widget.isMultiRecord && m[1].ids.includes(recordID)) {
-							self._reload(m[1], controller);
-						}
+				if((controller.widget)&&(controller.widget.modelName == 'board.board')){
+					self._reload(m[1], controller);
 				}
 			}
 		})
